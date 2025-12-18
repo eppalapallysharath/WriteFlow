6a2575267b4528b7822d042ec5dbf3c6cd9ba9f9
eppalapallysharath <eppalapallysharath@gmail.com>
2025-12-18 16:21:25 +0530

    added render url

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,2 +1,3 @@
-export const baseurl = "http://localhost:8081";
+// export const baseurl = "http://localhost:8081";
+export const baseurl = "https://writeflow-backend-pxdt.onrender.com";
 // export const {token} = JSON.parse(localStorage.getItem("auth"))
